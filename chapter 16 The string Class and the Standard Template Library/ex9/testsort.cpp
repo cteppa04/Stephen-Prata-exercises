22a7eb3f8b7fb8154286a00291c1ffe9f85c5024
Stefan <kleckovstepan7@gmail.com>
2025-10-16 17:14:37 +0200

    chapter 16 ex9 changes

--- chapter 16 The string Class and the Standard Template Library/ex9/testsort.cpp
+++ chapter 16 The string Class and the Standard Template Library/ex9/testsort.cpp
@@ -15,13 +15,13 @@ int main()
 	std::cout << "Enter size of array: ";
 	std::cin >> SIZE;
 	
-	std::vector<int> vi0(SIZE);
-	for (int i = 0; i < vi0.size(); i++)
-		vi0[i] = rand() % INT_MAX;
+	std::vector<int> vi0;
+	for (int i = 0; i < SIZE; i++)
+		vi0.push_back(rand() % INT_MAX);
 
 	std::vector<int> vi(SIZE);
-	std::copy(vi0.begin(), vi0.end(), vi.begin());
 	std::list<int> li(SIZE);
+	std::copy(vi0.begin(), vi0.end(), vi.begin());
 	std::copy(vi0.begin(), vi0.end(), li.begin());
 
 	std::cout << "Array size: " << SIZE << std::endl;
@@ -35,6 +35,14 @@ int main()
 	li.sort();
 	finish = clock();
 	std::cout << "Time to sort list array: " << (double)(finish - start) / CLOCKS_PER_SEC << " seconds" << std::endl;
+
+	li.assign(vi0.begin(), vi0.end());
+	start = clock();
+	vi.assign(li.begin(), li.end());
+	std::sort(vi.begin(), vi.end());
+	li.assign(vi.begin(), vi.end());
+	finish = clock();
+	std::cout << "Time to sort list array with vector sort: " << (double)(finish - start) / CLOCKS_PER_SEC << " seconds" << std::endl;
 
 	return 0;
 }
