c53399809856058b8d6413828799504d00568cd0
Stefan <kleckovstepan7@gmail.com>
2025-10-16 15:01:18 +0200

    chapter 16 ex8

---
+++ chapter 16 The string Class and the Standard Template Library/ex8/party.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+	std::list<std::string> Mat;
+	std::list<std::string> Pat;
+	std::list<std::string> All;
+	std::string temp;
+
+	std::cout << "Enter Mat's friends list(q to quit): ";
+	while (std::cin >> temp && temp != "q")
+	{
+		Mat.push_back(temp);
+	}
+	Mat.sort();
+	std::cout << "Sorted Mat's friends list:\n";
+	for (auto it = Mat.begin(); it != Mat.end(); it++)
+	{
+		std::cout << *it << "    ";
+	}
+	std::cout << std::endl << std::endl;
+
+	std::cout << "Enter Pat's friends list(q to quit): ";
+	while (std::cin >> temp && temp != "q")
+	{
+		Pat.push_back(temp);
+	}
+	Pat.sort();
+	std::cout << "Sorted Mat's friends list:\n";
+	for (auto it = Pat.begin(); it != Pat.end(); it++)
+	{
+		std::cout << *it << "    ";
+	}
+	std::cout << std::endl << std::endl;
+
+	for (std::list<std::string>::iterator it = Mat.begin(); it != Mat.end(); it++)
+	{
+		All.push_back(*it);
+	}
+	for (auto it = Pat.begin(); it != Pat.end(); it++)
+	{
+		All.push_back(*it);
+	}
+	All.sort();
+	All.unique();
+
+	std::cout << "List of all friends:\n";
+	for (auto it = All.begin(); it != All.end(); it++)
+	{
+		std::cout << *it << "    ";
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
